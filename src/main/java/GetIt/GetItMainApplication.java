14678892477c803c15a5758e69737873ae08f9ee
TomerDamri <tomerdamri800@gmail.com>
2020-08-21 09:20:06 +0300

    Revert

--- src/main/java/GetIt/GetItMainApplication.java
+++ src/main/java/GetIt/GetItMainApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages= {"GetIt.controller"})
 public class GetItMainApplication {
 
 	public static void main(String[] args) {
